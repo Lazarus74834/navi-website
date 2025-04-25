7f0f5c0b0b77146129c6386435426501cf4a9a3c
Lazarus74834 <55579624+Lazarus74834@users.noreply.github.com>
2025-04-25 12:48:47 +0100

    Fixes 2

--- vite.config.js
+++ vite.config.js
@@ -10,6 +10,13 @@ export default defineConfig({
   build: {
     outDir: 'dist',
     assetsInlineLimit: 0, // Disable asset inlining
+    rollupOptions: {
+      output: {
+        entryFileNames: 'assets/[name]-[hash].js',
+        chunkFileNames: 'assets/[name]-[hash].js',
+        assetFileNames: 'assets/[name]-[hash].[ext]'
+      }
+    }
   },
   resolve: {
     alias: {
